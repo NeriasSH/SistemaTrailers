733bc9037a9b4d3591e4b7893188e38bca77d81a
NeriasSH <nesh.nerias@gmail.com>
2024-11-29 18:25:31 -0500

    Versionfinal1.0

--- src/main/java/com/sistema/trailers/controladores/HomeControlador.java
+++ src/main/java/com/sistema/trailers/controladores/HomeControlador.java
@@ -2,6 +2,8 @@ package com.sistema.trailers.controladores;
 
 import java.util.List;
 
+import com.sistema.trailers.modelo.Administrador;
+import com.sistema.trailers.repositorios.AdministradorRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,13 +11,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sistema.trailers.modelo.Pelicula;
 import com.sistema.trailers.repositorios.PeliculaRepositorio;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("")
@@ -44,10 +47,8 @@ public class HomeControlador {
 		return new ModelAndView("pelicula").addObject("pelicula",pelicula);
 	}
 
-	@GetMapping("/loginadmin")
-	public String mostrarLoginAdmin() {
-		return "login"; // Nombre del archivo HTML dentro de templates
-	}
+
+
 
 
 }
